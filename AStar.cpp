1f6747a70dae21e87e6f7e97afd5fb5a12f96294
Nithin Gunamgari <59269423+nithin-gunamgari@users.noreply.github.com>
2020-12-13 17:01:30 -0600

    Add files via upload

---
+++ AStar.cpp
@@ -0,0 +1,182 @@
+#include "AStar.hpp"
+#include <algorithm>
+
+using namespace std::placeholders;
+
+bool AStar::coor::operator == (const coor& coordinates_)
+{
+    return (x == coordinates_.x && y == coordinates_.y);
+}
+
+AStar::coor operator + (const AStar::coor& left_, const AStar::coor& right_)
+{
+    return{ left_.x + right_.x, left_.y + right_.y };
+}
+
+AStar::Node::Node(coor coordinates_, Node *parent_)
+{
+    parent = parent_;
+    coordinates = coordinates_;
+    G = H = 0;
+}
+
+AStar::uint AStar::Node::getScore()
+{
+    return G + H;
+}
+
+AStar::maps::maps()
+{
+    setDiagonalMovement(false);
+    setHeuristic(&Heuristic::manhattan);
+    direction = {
+        { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 },
+        { -1, -1 }, { 1, 1 }, { -1, 1 }, { 1, -1 }
+    };
+}
+
+void AStar::maps::setWorldSize(coor worldSize_)
+{
+    worldSize = worldSize_;
+}
+
+void AStar::maps::setDiagonalMovement(bool enable_)
+{
+    directions = (enable_ ? 8 : 4);
+}
+
+void AStar::maps::setHeuristic(HeuristicFunction heuristic_)
+{
+    heuristic = std::bind(heuristic_, _1, _2);
+}
+
+void AStar::maps::addCollision(coor coordinates_)
+{
+    walls.push_back(coordinates_);
+}
+
+void AStar::maps::removeCollision(coor coordinates_)
+{
+    auto it = std::find(walls.begin(), walls.end(), coordinates_);
+    if (it != walls.end()) {
+        walls.erase(it);
+    }
+}
+
+void AStar::maps::clearCollisions()
+{
+    walls.clear();
+}
+
+AStar::CoordinateList AStar::maps::findPath(coor source_, coor target_)
+{
+    Node *current = nullptr;
+    NodeSet openSet, closedSet;
+    openSet.reserve(100);
+    closedSet.reserve(100);
+    openSet.push_back(new Node(source_));
+
+    while (!openSet.empty()) {
+        auto current_it = openSet.begin();
+        current = *current_it;
+
+        for (auto it = openSet.begin(); it != openSet.end(); it++) {
+            auto node = *it;
+            if (node->getScore() <= current->getScore()) {
+                current = node;
+                current_it = it;
+            }
+        }
+
+        if (current->coordinates == target_) {
+            break;
+        }
+
+        closedSet.push_back(current);
+        openSet.erase(current_it);
+
+        for (uint i = 0; i < directions; ++i) {
+            coor newCoordinates(current->coordinates + direction[i]);
+            if (detectCollision(newCoordinates) ||
+                findNodeOnList(closedSet, newCoordinates)) {
+                continue;
+            }
+
+            uint totalCost = current->G + ((i < 4) ? 10 : 14);
+
+            Node *successor = findNodeOnList(openSet, newCoordinates);
+            if (successor == nullptr) {
+                successor = new Node(newCoordinates, current);
+                successor->G = totalCost;
+                successor->H = heuristic(successor->coordinates, target_);
+                openSet.push_back(successor);
+            }
+            else if (totalCost < successor->G) {
+                successor->parent = current;
+                successor->G = totalCost;
+            }
+        }
+    }
+
+    CoordinateList path;
+    while (current != nullptr) {
+        path.push_back(current->coordinates);
+        current = current->parent;
+    }
+
+    releaseNodes(openSet);
+    releaseNodes(closedSet);
+
+    return path;
+}
+
+AStar::Node* AStar::maps::findNodeOnList(NodeSet& nodes_, coor coordinates_)
+{
+    for (auto node : nodes_) {
+        if (node->coordinates == coordinates_) {
+            return node;
+        }
+    }
+    return nullptr;
+}
+
+void AStar::maps::releaseNodes(NodeSet& nodes_)
+{
+    for (auto it = nodes_.begin(); it != nodes_.end();) {
+        delete *it;
+        it = nodes_.erase(it);
+    }
+}
+
+bool AStar::maps::detectCollision(coor coordinates_)
+{
+    if (coordinates_.x < 0 || coordinates_.x >= worldSize.x ||
+        coordinates_.y < 0 || coordinates_.y >= worldSize.y ||
+        std::find(walls.begin(), walls.end(), coordinates_) != walls.end()) {
+        return true;
+    }
+    return false;
+}
+
+AStar::coor AStar::Heuristic::getDelta(coor source_, coor target_)
+{
+    return{ abs(source_.x - target_.x),  abs(source_.y - target_.y) };
+}
+
+AStar::uint AStar::Heuristic::manhattan(coor source_, coor target_)
+{
+    auto delta = std::move(getDelta(source_, target_));
+    return static_cast<uint>(10 * (delta.x + delta.y));
+}
+
+AStar::uint AStar::Heuristic::euclidean(coor source_, coor target_)
+{
+    auto delta = std::move(getDelta(source_, target_));
+    return static_cast<uint>(10 * sqrt(pow(delta.x, 2) + pow(delta.y, 2)));
+}
+
+AStar::uint AStar::Heuristic::octagonal(coor source_, coor target_)
+{
+    auto delta = std::move(getDelta(source_, target_));
+    return 10 * (delta.x + delta.y) + (-6) * std::min(delta.x, delta.y);
+}
